7f46d98eae3ddb9b036f0f6c2f84621cfdb16571
John Sager <jsager@athenahealth.com>
2021-11-03 14:16:04 -0500

    added submit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,4 +1,5 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import App from "./App";
 
 describe("App", () => {
@@ -9,5 +10,28 @@ describe("App", () => {
   it("should render a submit button", () => {
     render(<App />);
     screen.getByRole("button", { name: "Send" });
+  });
+  it("should set aria-disabled on Send button when input is empty", () => {
+    render(<App />);
+    const sendButton = screen.getByRole("button", { name: "Send" });
+    expect(sendButton).toHaveAttribute("aria-disabled", "true");
+  });
+  it("should not be aria-disabled on Send button when input is populated", () => {
+    render(<App />);
+    const sendButton = screen.getByRole("button", { name: "Send" });
+    const input = screen.getByLabelText("Message");
+
+    userEvent.type(input, "example message");
+    expect(sendButton).toHaveAttribute("aria-disabled", "false");
+  });
+  it("should clear message input when Send is clicked", () => {
+    render(<App />);
+    const sendButton = screen.getByRole("button", { name: "Send" });
+    const input = screen.getByLabelText("Message");
+
+    userEvent.type(input, "example message");
+    userEvent.click(sendButton);
+
+    expect(input).toBeEmptyDOMElement();
   });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,43 @@
+import { useState } from "react";
 import Form from "@athena/forge/Form";
 import Root from "@athena/forge/Root";
 import Textarea from "@athena/forge/Textarea";
 import FormField from "@athena/forge/FormField";
+import Button from "@athena/forge/Button";
+import Heading from "@athena/forge/Heading";
 
 import "@athena/forge/dist/forge.css";
 
 const App = () => {
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState<string[]>([]);
+
   return (
     <Root>
-      <h1>Chat</h1>
-      <Form buttonText="Send">
-        <FormField id="message" labelText="Message" inputAs={Textarea} />
+      <Heading text="Chat" />
+
+      {messages.map((msg) => (
+        <p>{msg}</p>
+      ))}
+
+      <Form
+        buttonText="Send"
+        includeSubmitButton={false}
+        onSubmit={(event) => {
+          event.preventDefault();
+          setMessages([...messages, message]);
+          setMessage("");
+        }}
+      >
+        <FormField
+          id="message"
+          labelAlwaysAbove
+          labelText="Message"
+          inputAs={Textarea}
+          value={message}
+          onChange={(e) => setMessage(e.currentTarget.value)}
+        />
+        <Button type="submit" text="Send" disabled={!message} />
       </Form>
     </Root>
   );
